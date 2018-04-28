c59b8349a5066e08e0ee8483325b672b5bc56c31
shwetabadi <38793011+shwetabadi@users.noreply.github.com>
2018-04-28 11:43:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/', function (req, res) {
+app.get('/atrical1', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'artical1.html'));
 });
 
